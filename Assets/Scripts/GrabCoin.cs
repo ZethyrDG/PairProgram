b6a40ca3c46230a742c12f4b50a6a6a4c48f764d
David <banolanshadowsong@hotmail.com>
2019-02-26 14:00:04 -0800

    First commit

---
+++ Assets/Scripts/GrabCoin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GrabCoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entered");
+        this.gameObject.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingright = true;
+    private Rigidbody2D rb2d;
+
+bool grounded = false;
+public Transform groundCheck;
+float groundRadius = 0.2f;
+public LayerMask whatIsGround;
+public float jumpForce = 5f;
+
+// Start is called before the first frame update
+void Start()
+{
+    rb2d = GetComponent<Rigidbody2D>();
+}
+
+// Update is called once per frame
+void FixedUpdate()
+{
+    grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+
+    float move = Input.GetAxis("Horizontal");
+
+    rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+}
+
+private void Update()
+{
+    if (grounded && Input.GetKeyDown(KeyCode.Space)) 
+    {
+        rb2d.AddForce(new Vector2(0, jumpForce));
+    }
+}
+}
